feacb3a3eb645627f2a3ff17d7924eb9aa242a58
bielgeri <geribolarodrigues@gmail.com>
2026-09-14 08:26:04 -0300

    Course

--- backend/dslearnbds/src/main/resources/import.sql
+++ backend/dslearnbds/src/main/resources/import.sql
@@ -13,3 +13,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 3);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3XwZETxAnSDVf1nxkGXLa4mE1Dc674-Nj48RANEkJqJH6jXGqqGl2umM&s=10', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ9P6ZHnN4eeqIMswKJrPfldR7tuk817F89Rq05TDe5C3Ehsl6stCXPog&s=10');
